37be35b81a78bb4f5a2649c9c44a6bf40fc1d850
LuisCL94 <lclaudio.tl@gmail.com>
2020-11-02 00:48:49 -0300

    Obtendo barra verde após oferecer implementação para a unidade sob desenvolvimento - rendimento bruto
    Co-authored-by: jps12 <jps12carvalho@gmail.com>
    Co-authored-by: RenatoBrittoAraujo <renatobritto@protonmail.com>

--- src/app/CDB.java
+++ src/app/CDB.java
@@ -1,7 +1,20 @@
 package app;
+import java.lang.Math;
+import java.text.DecimalFormat;
 
 public class CDB {
 	public float redimentoBruto(int dias, float aplicacaoInicial, float txAnual) {
-		return 13.97f;
+		float tx = txAnual/100f;
+		float ano = dias/365f;
+		
+		float valorFinal = (float) (aplicacaoInicial * (1 + tx * ano));
+		
+		float total = valorFinal - aplicacaoInicial;
+		
+		total = (int) (total*100f);
+		total = total/100f;
+		
+		System.out.println(total);
+		return total;
 	}
 }
