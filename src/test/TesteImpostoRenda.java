e543f46b7278f9e2141db9abf2f17fb7680faad2
Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
2020-11-02 16:29:07 -0300

    3 - Imposto de renda barra vermelha por novo teste

--- src/test/TesteImpostoRenda.java
+++ src/test/TesteImpostoRenda.java
@@ -21,4 +21,10 @@ public class TesteImpostoRenda{
 		cdb.redimentoBruto(60, 1000f, 8.5f);
 		assertEquals(3.14f, cdb.getImposto(),0.01);	
    }
+   
+   @Test
+	public void testCalcularImpostoRenda2() {
+		cdb.redimentoBruto(350,800f,8.0f);
+		assertEquals(12.27f, cdb.getImposto(), 0.01);
+	}
 }
